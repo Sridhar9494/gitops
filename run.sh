568eeb88219f832c3ed30cf5f045d6ffb95f5763
sri9494 <63033263+Sridhar9494@users.noreply.github.com>
2021-12-31 17:21:00 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 git config --global user.email  $GITHUB_USERNAME
 git config --global user.name $GITHUB_USER_EMAIL
-git clone https://$4@github.com/Fantasy-ENV.git  $RUNNER_TEMP/Fantasy-ENV
+git clone https://Sridhar9494:$GITHUB_TOKEN@github.com/Fair-Play-Club/Fantasy-ENV.git  $RUNNER_TEMP/Fantasy-ENV
 wget https://raw.githubusercontent.com/Sridhar9494/gitops/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/Fantasy-ENV/$1 --key $2 --value $3
 cd $RUNNER_TEMP/Fantasy-ENV
