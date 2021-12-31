1f4c8f54480654e77619049a81c3722d1f50653f
sri9494 <63033263+Sridhar9494@users.noreply.github.com>
2021-12-31 17:37:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ git config --global user.email  $GITHUB_USERNAME
 git config --global user.name $GITHUB_USER_EMAIL
 git clone https://$4@github.com/$5.git  $RUNNER_TEMP/infra-as-code-repo
 wget https://raw.githubusercontent.com/Sridhar9494/gitops/master/replace-key.py
-python replace-key.py --file $RUNNER_TEMP/Fantasy-ENV/$1 --key $2 --value $3
-cd $RUNNER_TEMP/Fantasy-ENV
+python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$1 --key $2 --value $3
+cd $RUNNER_TEMP/infra-as-code-repo
 git add .
 git commit -m"Release of key $2 in $1"
 git push
