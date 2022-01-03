fd932f93af06f247f0a0d9676186abbe3c188c1e
sri9494 <63033263+Sridhar9494@users.noreply.github.com>
2022-01-03 11:31:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 git config --global user.email  $GITHUB_USERNAME
 git config --global user.name $GITHUB_USER_EMAIL
-git clone https://$4@github.com/$5.git  $RUNNER_TEMP/infra-as-code-repo
+git clone https://Sridhar9494:$4@github.com/$5.git  $RUNNER_TEMP/infra-as-code-repo
 wget https://raw.githubusercontent.com/DenisPalnitsky/gitops-release/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/deployment.apps/$1 --key $2 --value $3
 cd $RUNNER_TEMP/infra-as-code-repo
